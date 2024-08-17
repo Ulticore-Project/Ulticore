635ef675733cb1de45f70b87c38dae63be1aa9ad
Sunch <120295462+Sunch233@users.noreply.github.com>
2024-08-17 19:26:20 +0800

    workflows: add Integration tests

---
+++ tests/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "version\nstop\n" | php -dphar.readonly=0 PocketMine-MP.php --no-wizard --disable-ansi --disable-readline --debug.level=2
+
+log_file="console.log"
+output=$(cat "$log_file")
+
+if [ $(grep -c "ERROR\|CRITICAL\|EMERGENCY" <<< "$output") -ne 0 ]; then
+	echo Server log contains error messages, changing build status to failed
+	exit 1
+else
+	echo Server running correctly
+fi
+
+echo All tests passed
