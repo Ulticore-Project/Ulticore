ec3a8c134bc3115ed01fa51cdc97b4ea442b93d6
GameHerobrine <waterfall009@gmail.com>
2025-05-28 13:10:00 +0300

    better inventory sync

--- src/network/MinecraftInterface.php
+++ src/network/MinecraftInterface.php
@@ -4,49 +4,51 @@ class MinecraftInterface{
 
 	public $bandwidth;
 	private $socket;
-    private $UDPserver;
 	private $packets;
 	public $start;
 	function __construct($server, $port = 25565, $serverip = "0.0.0.0"){
-        $this->UDPserver = new UDPServer($server, $port, true, $serverip);
-
+		$this->socket = new UDPSocket($server, $port, true, $serverip);
+		if($this->socket->connected === false){
+			console("[SEVERE] Couldn't bind to $serverip:" . $port, true, true, 0);
+			exit(1);
+		}
 		$this->bandwidth = [0, 0, microtime(true)];
 		$this->start = microtime(true);
 		$this->packets = [];
 	}
 
 	public function close(){
-        $this->UDPserver->shutdown();
+		return $this->socket->close(false);
 	}
 
 	public function readPacket(){
-		if($this->UDPserver->shutdown === true){
+		if($this->socket->connected === false){
 			return false;
 		}
 		$buf = "";
 		$source = false;
 		$port = 1;
-		$len = $this->UDPserver->readThreadToMainPacket($buf, $source, $port);
+		$len = $this->socket->read($buf, $source, $port);
 		if($len === false or $len === 0){
-			return -1;
+			return false;
 		}
 		$this->bandwidth[0] += $len;
 		ServerAPI::request()->api->dhandle("mcinterface.read", ["buffer" => $buf, "source" => $source, "port" => $port]);
-        return $this->parsePacket($buf, $source, $port);
+		return $this->parsePacket($buf, $source, $port);
 	}
 
 	private function parsePacket($buffer, $source, $port){
 		$pid = ord($buffer[0]);
 
 		if(RakNetInfo::isValid($pid)){
-			$parser = new RakNetParser($buffer);
-			if($parser->packet !== false){
-				$parser->packet->ip = $source;
-				$parser->packet->port = $port;
-				if(EventHandler::callEvent(new PacketReceiveEvent($parser->packet)) === BaseEvent::DENY){
+			$packet = RakNetParser::parse($buffer);
+			if($packet !== false){
+				$packet->ip = $source;
+				$packet->port = $port;
+				if(EventHandler::callEvent(new PacketReceiveEvent($packet)) === BaseEvent::DENY){
 					return false;
 				}
-				return $parser->packet;
+				return $packet;
 			}
 			return false;
 		}elseif($pid === 0xfe and $buffer[1] === "\xfd" and ServerAPI::request()->api->query instanceof QueryHandler){
@@ -72,10 +74,10 @@ class MinecraftInterface{
 		if(EventHandler::callEvent(new PacketSendEvent($packet)) === BaseEvent::DENY){
 			return 0;
 		}elseif($packet instanceof RakNetPacket){
-			$codec = new RakNetCodec($packet);
+			RakNetCodec::encode($packet);
 		}
-		$this->UDPserver->pushMainToThreadPacket($packet->buffer, $packet->ip, $packet->port);
-		$this->bandwidth[1] += strlen($packet->buffer);
-        return true;
+		$write = $this->socket->write($packet->buffer, $packet->ip, $packet->port);
+		$this->bandwidth[1] += $write;
+		return $write;
 	}
 }
